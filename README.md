87dc167bf98f8fb3ef37defe43b1eb0cdab07dc1
Ali Dandach <alidandach1995@gmail.com>
2021-04-30 20:43:16 +0300

    control the input

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Conway-s-Game-of-Life
+# Conway's Game of Life
 The Game of Life, also known simply as Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970
 
 ### The Rules:
--- conway_game_of_life.py
+++ conway_game_of_life.py
@@ -2,18 +2,24 @@ from cellular_automaton import CA
 
 
 def play():
-    number_of_rows = int(input('please enter the number of rows'))
-    number_of_columns = int(input('please enter the number of columns'))
+    number_of_rows = int(input('please enter the number of rows (must be less than or equal 10):'))
+    number_of_columns = int(input('please enter the number of columns (must be less than or equal 10):'))
+
+    if number_of_rows <= 0 or number_of_columns <= 0:
+        raise ValueError('number of row or column must positive')
+
+    if number_of_rows > 10 and number_of_columns > 10:
+        raise ValueError('number of row or column must be less than or equal 10')
 
     game_of_life_board = CA(number_of_rows, number_of_columns)
 
     game_of_life_board.print_automata()
 
-    user_action = ''
-    while user_action != 'x':
-        user_action = input('if you want to stop please press x or you can continue using enter button')
+    control_command = ''
+    while control_command != 'x':
+        control_command = input('if you want to stop please press x or you can continue using enter button')
 
-        if user_action == '':
+        if control_command == '':
             game_of_life_board.get_next_generation()
             game_of_life_board.print_automata()
 
